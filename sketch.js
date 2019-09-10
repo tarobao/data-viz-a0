5eada2cd525e5c6821a66cdc052f3b3bb065f1ec
unknown <ell2140@columbia.edu>
2019-09-09 22:51:58 -0400

    for some reason sketch.js didn't save my changes

--- sketch.js
+++ sketch.js
@@ -4,16 +4,12 @@ let hd, md, sd;
 let hMax, hMin, mMax, mMin;
 
 function setup() {
-  createCanvas(500, 500); // make an HTML canvas element width x height pixels
+  createCanvas(1000, 700); // make an HTML canvas element width x height pixels
   // setting starting colors 
   hc = color(random(255), random(255), random(255));
   mc = color(random(255), random(255), random(255));
   sc = color(random(255), random(255), random(255));
   frameRate(1);
-  
-  h = 0;
-  m = 30;
-  s = 15;
 
   /* unfortunately, I made a super amateur design mistake and made the diameters of each ellipse dependent on each other! so these variables ensure that hour 0 never causes nothing to show. */
   
@@ -24,14 +20,6 @@ function setup() {
 
 function draw() {
   background(225);
-  
-  textSize(32);
-  fill(180);
-  text(hour(), 10, 30);
-  fill(100);
-  text(minute(), 10, 60);
-  fill(0);
-  text(second(), 10, 90);
 
   rc = color(random(255), random(255), random(255));
 
@@ -43,9 +31,10 @@ function draw() {
   hd = map(h, 0, 24, hMin, hMax);
   md = map(m, 0, 60, hd/10, hd);
   sd = map(s, 0, 60, md/10, md);
-  text(hd, width/2, 30);
-  text(md, width/2, 60);
-  text(sd, width/2, 90);
+
+  // text(hd, width/2, 30);
+  // text(md, width/2, 60);
+  // text(sd, width/2, 90);
 
   noStroke();
   // hour
